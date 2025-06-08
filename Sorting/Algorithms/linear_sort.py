dbe45e57cc738210cf1f711f09f2bf8ced219471
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-06-08 16:04:43 +0200

    Fixing an issue

--- Sorting/Algorithms/linear_sort.py
+++ Sorting/Algorithms/linear_sort.py
@@ -3,19 +3,18 @@ def linear_sort(array):
     max_element = max(array)
     
     placement = [0] * ((max_element - min_element) + 1)
+
     for j in range(min_element, max_element + 1):
         placement[j - min_element] = min_element - 1
-    
+
     for i in range(len(array)):
         key = array[i]
         placement[key - min_element] = key
-    
+
     i = 0
     for j in range(min_element, max_element + 1):
         if placement[j - min_element] >= min_element:
             array[i] = placement[j - min_element]
             i += 1
-    
+            
     return array
-
-print(linear_sort([6, 2, 1, 3, 0]))
