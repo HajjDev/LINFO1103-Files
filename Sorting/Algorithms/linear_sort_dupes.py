2ac7914f75e129679fe5b2f156b812367c421fdb
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-06-08 20:20:53 +0200

    Added counting sort

--- Sorting/Algorithms/linear_sort_dupes.py
+++ Sorting/Algorithms/linear_sort_dupes.py
@@ -3,19 +3,18 @@ def linear_sort(array):
     max_element = max(array)
     
     placement = [0] * ((max_element - min_element) + 1)
-    for j in range(min_element, max_element + 1):
-        placement[j - min_element] = min_element - 1
-        
     for j in range(len(array)):
         key = array[j]
-        placement[key - min_element] = key
+        placement[key - min_element] += 1
         
     i = 0
     for j in range(min_element, max_element + 1):
-        if placement[j - min_element] >= min_element:
-            array[i] = placement[j - min_element]
+        count = placement[j - min_element]
+        while count > 0:
+            array[i] = j
             i += 1
+            count -= 1
     
     return array
 
-print(linear_sort([2, 0, 1, 5]))
+print(linear_sort([2, 2, 0, 1, 5]))
